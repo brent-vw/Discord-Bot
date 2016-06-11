577825bd9078f2830da4c23b53f4f5f93bd0e420
brent <brent.van.wynsberge@gmail.com>
2016-06-11 23:05:55 +0200

    Documentation

--- main.py
+++
@@ -1,8 +0,0 @@
-"""
-Initializes the GanjaBot with apikeys and starts it.
-Will accept command line arguments in a future release.
-"""
-import ganja
-
-client = ganja.GanjaClient('.apikeys')
-client.run()
