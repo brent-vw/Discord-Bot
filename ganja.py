8cc6c89e87d1b9bd05793be9bdc5b0e3c00f35f5
Brent Van Wynsberge <brent.van.wynsberge@gmail.com>
2017-03-06 13:43:02 +0100

    hello kitty island

--- ganja.py
+++ ganja.py
@@ -472,15 +472,15 @@ Extends the Client class in discord.py.
                     self.queue.put(url)
                     title = get_vid_title(url)
                     self.queue_name.put(title)
-            if self.voice:
-                if self.player:
-                    if self.player.is_playing():
-                        self.player.stop()
-            self.voice = yield from self.join_voice_channel(channel)
-            self.voic.opus.load_opus()
-            self.player = yield from self.voice.create_ytdl_player(self.queue.get())
-            self.player.volume = 0.05
-            self.player.start()
+                if self.voice:
+                    if self.player:
+                        if self.player.is_playing():
+                            self.player.stop()
+                self.voice = yield from self.join_voice_channel(channel)
+                self.voice.opus.load_opus()
+                self.player = yield from self.voice.create_ytdl_player(self.queue.get())
+                self.player.volume = 0.05
+                self.player.start()
 
 
 class PlayThread(threading.Thread):
